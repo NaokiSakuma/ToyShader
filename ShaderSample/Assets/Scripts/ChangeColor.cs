12297daa789524d63384fc67fb96509f20235e5d
na-sakuma <naoki.sakuma@10antz.co.jp>
2019-05-16 18:04:36 +0900

    プロパティの追加

---
+++ ShaderSample/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour{
+    void Start(){
+        // shaderで定義した、_BaseColor変数の色を変更する
+        GetComponent<Renderer>().material.SetColor("_BaseColor",Color.yellow);
+    }
+}
