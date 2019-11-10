2e7ab1795652c0192e757477cc1203d4d9dd9f1e
sakuma <soramamenatan@gmail.com>
2019-11-10 17:40:34 +0900

    tekitou

--- ShaderSample/Assets/Scripts/Monochrome.cs
+++ ShaderSample/Assets/Scripts/Monochrome.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Monochrome : MonoBehaviour {
 	[SerializeField]
     private Shader _shader;
+<<<<<<< HEAD
 	[SerializeField]
     private Material _material;
 
 	void Start() {
 		if(_material == null)
 			_material = new Material(_shader);
+=======
+    private Material _material;
+
+	void Start() {
+		_material = new Material(_shader);
+>>>>>>> 1f3912dfbbb6ad6eb0dbc49c6f22cb064233ad12
 	}
 	void OnRenderImage(RenderTexture source, RenderTexture dest) {
 		Graphics.Blit(source, dest, _material);
