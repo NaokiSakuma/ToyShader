cd0e14d502118c82f327c681fcfa7ea27e0fcbf2
NaokiSakuma <soramamenatan@gmail.com>
2020-02-02 19:51:58 +0900

    雪のstart関数までコメント

--- ShaderSample/Assets/Scripts/Snow.cs
+++ ShaderSample/Assets/Scripts/Snow.cs
@@ -2,6 +2,7 @@
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Snow : MonoBehaviour {
+	// ドローコールの限界/4
 	private const int SNOW_NUM = 16000;
 	private Vector3[] _vertices;
 	private int[] _triangles;
@@ -12,32 +13,35 @@ public class Snow : MonoBehaviour {
 	private Vector3 _move;
 
 	void Start() {
-// 雪を降らせる範囲
-_range = 16.0f;
-// _rangeの逆数
-_rangeR = 1.0f / _range;
-// 雪の頂点
-_vertices = new Vector3[SNOW_NUM * 4];
-
-for (int i = 0; i < SNOW_NUM; i++) {
-	float x = Random.Range(-_range, _range);
-	float y = Random.Range(-_range, _range);
-	float z = Random.Range(-_range, _range);
-	var point = new Vector3(x, y, z);
-	_vertices[i * 4 + 0] = point;
-	_vertices[i * 4 + 1] = point;
-	_vertices[i * 4 + 2] = point;
-	_vertices[i * 4 + 3] = point;
-}
+		// 雪を降らせる範囲
+		_range = 16.0f;
+		// _rangeの逆数
+		_rangeR = 1.0f / _range;
+		// 雪の頂点
+		_vertices = new Vector3[SNOW_NUM * 4];
+		// 頂点の位置
+		for (int i = 0; i < SNOW_NUM; i++) {
+			float x = Random.Range(-_range, _range);
+			float y = Random.Range(-_range, _range);
+			float z = Random.Range(-_range, _range);
+			var point = new Vector3(x, y, z);
+			_vertices[i * 4 + 0] = point;
+			_vertices[i * 4 + 1] = point;
+			_vertices[i * 4 + 2] = point;
+			_vertices[i * 4 + 3] = point;
+		}
+		// 頂点を結ぶ順番
 		_triangles = new int[SNOW_NUM * 6];
 		for (int i = 0; i < SNOW_NUM; i++) {
-			_triangles[i * 6 + 0] = i * 4 + 0;
-			_triangles[i * 6 + 1] = i * 4 + 1;
-			_triangles[i * 6 + 2] = i * 4 + 2;
-			_triangles[i * 6 + 3] = i * 4 + 2;
-			_triangles[i * 6 + 4] = i * 4 + 1;
-			_triangles[i * 6 + 5] = i * 4 + 3;
+		_triangles[i * 6 + 0] = i * 4 + 0;
+		_triangles[i * 6 + 1] = i * 4 + 1;
+		_triangles[i * 6 + 2] = i * 4 + 2;
+		// ここで分割
+		_triangles[i * 6 + 3] = i * 4 + 2;
+		_triangles[i * 6 + 4] = i * 4 + 1;
+		_triangles[i * 6 + 5] = i * 4 + 3;
 		}
+		// UV値
 		_uvs = new Vector2[SNOW_NUM * 4];
 		for (int i = 0; i < SNOW_NUM; i++) {
 			_uvs[i * 4 + 0] = new Vector2(0.0f, 0.0f);
@@ -45,13 +49,13 @@ for (int i = 0; i < SNOW_NUM; i++) {
 			_uvs[i * 4 + 2] = new Vector2(0.0f, 1.0f);
 			_uvs[i * 4 + 3] = new Vector2(1.0f, 1.0f);
 		}
-
+		// Meshの生成
 		Mesh mesh = new Mesh();
 		mesh.name = "MeshSnowFlakes";
 		mesh.vertices = _vertices;
 		mesh.triangles = _triangles;
 		mesh.uv = _uvs;
-		// カメラの方向を取得するのに必要？
+		// 視錐台カリングを無効化
 		mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 99999999);
 		var mf = GetComponent<MeshFilter>();
 		mf.sharedMesh = mesh;
