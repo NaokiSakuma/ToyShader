7c84cf2105cbed1df79d103df3953bc37eef99a4
NaokiSakuma <soramamenatan@gmail.com>
2020-12-03 06:36:32 +0900

    鉛筆風ノイズ

---
+++ Assets/Shaders/PencilNoise/Scripts/PencilNoise.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PencilNoise : MonoBehaviour {
+
+    [SerializeField]
+    private Material _material;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest) {
+        Graphics.Blit(src, dest, _material);
+    }
+}
