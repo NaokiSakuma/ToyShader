5896d8a93b1ba5a864581bf9b78e76a9623df922
NaokiSakuma <soramamenatan@gmail.com>
2021-07-14 08:40:40 +0900

    射影テクスチャマッピング

---
+++ Assets/Shaders/ProjectionTexture/TextureProjecor.shader
@@ -0,0 +1,61 @@
+﻿Shader "Unlit/TextureProjecor"
+{
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float4 normal : NORMAL;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float4 projectorSpacePos : TEXCOORD0;
+                float3 worldPos : TEXCOORD1;
+                float3 worldNormal : TEXCOORD2;
+            };
+            
+            sampler2D _ProjectorTexture;
+            float4x4 _ProjectorMatrixVP;
+            float4 _ProjectorPos;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.projectorSpacePos = ComputeScreenPos(mul(mul(_ProjectorMatrixVP, unity_ObjectToWorld), v.vertex));
+                o.worldNormal = UnityObjectToWorldNormal(v.normal);
+                o.worldPos = mul(unity_ObjectToWorld, v.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // Projection座標に変換
+                // tex2DProj(_ProjectorTexture, i.projectorSpacePos)と同義
+                i.projectorSpacePos.xyz /= i.projectorSpacePos.w;
+                float4 projectorTex = tex2D(_ProjectorTexture, i.projectorSpacePos.xy);
+                // カメラの範囲外(0 ~ 1)は描画しない
+                fixed3 isOut = step((i.projectorSpacePos - 0.5) * sign(i.projectorSpacePos), 0.5);
+                float alpha = isOut.x * isOut.y * isOut.z;
+                // プロジェクターから見て、裏側の面には描画しない
+                // Perspective  : _ProjectorPos.xyz - i.worldPos
+                // Orthographic : -_ProjectorPos.xyz
+                alpha *= step(-dot(lerp(-_ProjectorPos.xyz, _ProjectorPos.xyz - i.worldPos, _ProjectorPos.w), i.worldNormal), 0);
+                return projectorTex * alpha;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Shaders/ProjectionTexture/TextureProjector.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+
+public class TextureProjector : MonoBehaviour
+{
+    [SerializeField, Range(0.0001f, 180)]
+    private float _fieldOfView = 60;
+    [SerializeField, Range(0.2f, 5.0f)]
+    private float _aspect = 1.0f;
+    [SerializeField, Range(0.0001f, 1000.0f)]
+    private float _nearClipPlane = 0.01f;
+    [SerializeField, Range(0.0001f, 1000.0f)]
+    private float _farClipPlane = 100.0f;
+    [SerializeField]
+    private ProjectionType projectionType;
+    [SerializeField]
+    private float _orthographicSize = 1.0f;
+    [SerializeField]
+    private Texture2D _texture;
+    [SerializeField]
+    private Material _material;
+
+    private enum ProjectionType
+    {
+        PERSPECTIVE,
+        ORTHOGRAPHIC,
+    }
+    
+    private int _matrixVpId;
+    private int _textureId;
+    private int _posId;
+    
+    private void Awake()
+    {
+        SetPropertyId();
+    }
+
+    /// <summary>
+    /// PropertyIdの設定
+    /// </summary>
+    private void SetPropertyId()
+    {
+        _matrixVpId = Shader.PropertyToID("_ProjectorMatrixVP");
+        _textureId = Shader.PropertyToID("_ProjectorTexture");
+        _posId = Shader.PropertyToID("_ProjectorPos");
+    }
+
+    private void Update()
+    {
+        SetMaterialParam();
+    }
+
+    /// <summary>
+    /// マテリアルのパラメータ設定
+    /// </summary>
+    private void SetMaterialParam()
+    {
+        if (_texture == null)
+        {
+            Debug.LogError("Not Setting Material.");
+            return;
+        }
+        var viewMatrix = Matrix4x4.Scale(new Vector3(1, 1, -1)) * transform.worldToLocalMatrix;
+        var projectionMatrix = GetProjectionMatrix();
+        _material.SetMatrix(_matrixVpId, projectionMatrix * viewMatrix);
+        _material.SetTexture(_textureId, _texture);
+        // プロジェクターの位置を渡す
+        // _ObjectSpaceLightPosのような感じでwに0が入っていたらOrthographicの前方方向とみなす
+        _material.SetVector(_posId, GetProjectorPos());
+    }
+
+    /// <summary>
+    /// Projection行列の取得
+    /// </summary>
+    /// <returns></returns>
+    private Matrix4x4 GetProjectionMatrix()
+    {
+        Matrix4x4 projectionMatrix;
+        switch (projectionType)
+        {
+            case ProjectionType.PERSPECTIVE :
+                projectionMatrix = Matrix4x4.Perspective(_fieldOfView, _aspect, _nearClipPlane, _farClipPlane);
+                break;
+            case ProjectionType.ORTHOGRAPHIC :
+               var orthographicWidth = _orthographicSize * _aspect;
+               projectionMatrix = Matrix4x4.Ortho(-orthographicWidth, orthographicWidth, -_orthographicSize, _orthographicSize, _nearClipPlane, _farClipPlane);
+               break;
+           default :
+               Debug.LogError("Not Expected CameraType");
+               return Matrix4x4.identity;
+        }
+        return GL.GetGPUProjectionMatrix(projectionMatrix, true);
+    }
+
+    /// <summary>
+    /// プロジェクターの座標取得
+    /// </summary>
+    /// <returns></returns>
+    private Vector4 GetProjectorPos()
+    {
+        Vector4 projectorPos;
+        switch (projectionType)
+        {
+            case ProjectionType.PERSPECTIVE :
+                projectorPos = transform.position;
+                projectorPos.w = 1;
+                break;
+            case ProjectionType.ORTHOGRAPHIC :
+                projectorPos = transform.forward;
+                projectorPos.w = 0;
+                break;
+            default :
+                Debug.LogError("Not Expected CameraType");
+                return Vector4.zero;
+        }
+        return projectorPos;
+    }
+    
+    private void OnDrawGizmos()
+    {
+        var gizmosMatrix = Gizmos.matrix;
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, Vector3.one);
+
+        switch (projectionType)
+        {
+            case ProjectionType.ORTHOGRAPHIC :
+                var orthographicWidth = _orthographicSize * _aspect;
+                var length = _farClipPlane - _nearClipPlane;
+                var start = _nearClipPlane + length / 2;
+                Gizmos.DrawWireCube(Vector3.forward * start, new Vector3(orthographicWidth * 2, _orthographicSize * 2, length));
+                break;
+            case ProjectionType.PERSPECTIVE :
+                Gizmos.DrawFrustum(Vector3.zero, _fieldOfView, _farClipPlane, _nearClipPlane, _aspect);
+                break;
+            default :
+                Debug.LogError("Not Expected CameraType");
+                break;
+        }
+
+        Gizmos.matrix = gizmosMatrix;
+    }
+}
